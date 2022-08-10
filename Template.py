b65554ca565cbfea21d172003d094406507fe066
Zombiebattler <82572053+Zombiebattler@users.noreply.github.com>
2022-08-11 00:38:26 +0200

    Update Template.py

--- Template.py
+++ Template.py
@@ -1,10 +1,18 @@
 import telebot
 
-API_KEY = ("your api key")
-bot = telebot.TeleBot(API_KEY)
+#set your variables
+API_KEY = ("Your API")          
+bot = telebot.TeleBot(API_KEY)   
 
+#First message
+@bot.message_handler(commands=['hi'])
+def hi(message):
+  bot.reply_to(message, "Hall")
+
+#second message
 @bot.message_handler(commands=['ping'])
 def Ping(message):
   bot.send_message(message.chat.id, 'Pong')
-  
+
+#starts the bot
 bot.polling()
